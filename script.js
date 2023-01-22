51a50c788cafdc803c72a3256dfc230306d55680
Charlie Cohen <charliecohen@Charlies-MacBook-Air.local>
2023-01-22 11:07:51 +0000

    starting colour

--- script.js
+++ script.js
@@ -2,9 +2,34 @@ const colourPicker = () => {
   const hue = Math.floor(Math.random() * 360);
   const saturation = Math.floor(Math.random() * (80 - 40 + 1) + 40);
   const light = Math.floor(Math.random() * (80 - 40 + 1) + 40);
-  return "hsl(" + hue + ", " + saturation + "%, " + light + "%)";
+  return { hue: hue, saturation: saturation, light: light };
 };
 
-const shadeGenerator = () => {};
+const lighterShades = (colour) => {
+  const { hue, saturation, light } = colour;
+};
+
+const generateShades = (baseColour) => {
+  lighterShades(baseColour);
+  const darkerShades = darkerShades(baseColour);
+};
+
+// const colourDisplay = (colour) => {
+//   const hslColour =
+//     "hsl(" +
+//     colour[0].hue +
+//     ", " +
+//     colour[0].saturation +
+//     "%, " +
+//     colour[0].light +
+//     "%)";
+//   var currentDiv = document.getElementById("container");
+//   var newDiv = document.createElement("div");
+//   newDiv.classList.add("newDivClass");
+//   currentDiv.appendChild(newDiv);
+//   newDiv.style.background = hslColour;
+//   console.log(newDiv);
+// };
 
 const baseColour = colourPicker();
+generateShades(baseColour);
